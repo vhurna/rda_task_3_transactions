198338b50bbda06d5930361a0afadfbda4d8df98
mira <mkyshchak@gmail.com>
2025-03-12 23:36:30 +0200

    'task.sql.3'

--- task.sql
+++ task.sql
@@ -1,11 +1,51 @@
--- Use our database
-USE ShopDB; 
+CREATE DATABASE IF NOT EXISTS ShopDB;
 
--- Some data should be created outside the transaction (here)
+USE ShopDB;
 
--- Start the transaction 
-START TRANSACTION; 
+CREATE TABLE IF NOT EXISTS Products (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(50),
+    Description VARCHAR(100),
+    Price DECIMAL(10, 2),
+    WarehouseAmount INT
+);
 
--- And some data should be created inside the transaction 
+CREATE TABLE IF NOT EXISTS Customers (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Address VARCHAR(100)
+);
 
-COMMIT; 
+CREATE TABLE IF NOT EXISTS Orders (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerID INT NOT NULL,
+    Date DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS OrderItems (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT NOT NULL,
+    ProductID INT NOT NULL,
+    Count INT NOT NULL,
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE CASCADE,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE CASCADE
+);
+
+START TRANSACTION;
+
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2023-01-01');
+
+SET @OrderID = LAST_INSERT_ID();
+
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (@OrderID, 1, 1);  
+
+UPDATE Products
+SET WarehouseAmount = WarehouseAmount - 1
+WHERE ID = 1;  
+
+COMMIT;
